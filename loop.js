4f880e1d7ff12a512f37c1b9a6161a68d35d04f6
kruskal <11516889+General-code@users.noreply.github.com>
2021-01-14 15:21:58 +0900

    Create loop.js

---
+++ loop.js
@@ -0,0 +1,34 @@
+let todos = [
+  'clean room',
+  'brush teeth',
+  'exercise',
+  'study javascript',
+  'eat healthy!',
+];
+
+// Warning!!! Wrong code
+// for(let i = 0; i < todos.length; i++){
+//     todos.pop();
+// }
+
+// above code should be written in this way
+// let todoLength = todos.length;
+// for (let i = 0; i < todos.length; ++i) {
+//   todos.pop();
+// }
+// Cause todos.length affected by the states of current todos.
+
+// let counterOne = 0;
+
+// while (counterOne < 10) {
+//   console.log(counterOne);
+//   counterOne++;
+// }
+
+// let coutnerTwo = 10;
+
+// do {
+//   console.log(coutnerTwo);
+//   coutnerTwo++;
+// } while (coutnerTwo < 10);
+// Check a condition after an execution
